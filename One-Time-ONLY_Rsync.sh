70777d03621ef24706fe4d95831ea3c59677c007
w-mccarty <94848975+w-mccarty@users.noreply.github.com>
2023-10-08 10:27:08 -0400

    Update One-Time-ONLY_Rsync.sh

--- One-Time-ONLY_Rsync.sh
+++ One-Time-ONLY_Rsync.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -
 #
-#Syncs remote directory to local directory and logs files to prevent any future resync of same files.  
+#Syncs remote directory to local directory and logs files to EXCLUDE future resync of the same files.  Useful if moving/editing files in local dir after syncing.
 #
 #vars
 localfiledir='/local/fileDirectory'
@@ -8,18 +8,18 @@ locallogdir='/local/logDirectory'
 remotedir='/remote/fileDirectory'
 remoteuser='username'
 remoteIP='192.168.0.1'
+#
 sedfiledir=$(echo "${localfiledir////$'\/'}")
 date=$(date +%Y-%m-%d_%H:%M:%S)
-#
 #first-run logging
 if [[ ! -e ${locallogdir}/nosyncfinal.txt ]]; then
 	touch ${locallogdir}/nosyncfinal.txt
 	find ${localfiledir}/ -maxdepth 1 -type f > ${locallogdir}/files.txt #list files
-	sed -i "s/$sedfiledir\///g" ${locallogdir}/files.txt
-	sed -i '/^$/d' ${locallogdir}/files.txt
+	sed -i "s/$sedfiledir\///g" ${locallogdir}/files.txt #filename only
+	sed -i '/^$/d' ${locallogdir}/files.txt #remove whitespace
 	find ${localfiledir}/ -maxdepth 1 -type d > ${locallogdir}/dirs.txt #list directories
-	sed -i "s/$sedfiledir\///g" ${locallogdir}/dirs.txt
-	sed -i '/^$/d' ${locallogdir}/dirs.txt
+	sed -i "s/$sedfiledir\///g" ${locallogdir}/dirs.txt #dirname only
+	sed -i '/^$/d' ${locallogdir}/dirs.txt #remove whitespace
 	cat ${locallogdir}/files.txt ${locallogdir}/dirs.txt > ${locallogdir}/nosync.txt #combine
 	awk '!seen[$0]++' ${locallogdir}/nosync.txt > ${locallogdir}/nosyncfinal.txt #remove duplicates
 	cp ${locallogdir}/nosyncfinal.txt ${locallogdir}/initsync_${date}.txt #initial log
@@ -31,11 +31,11 @@ rsync -a --progress --exclude-from "${locallogdir}/nosyncfinal.txt" ${remoteuser
 #
 #logging to prevent re-sync
 find ${localfiledir}/ -maxdepth 1 -type f > ${locallogdir}/files.txt #list files
-sed -i "s/$sedfiledir\///g" ${locallogdir}/files.txt
-sed -i '/^$/d' ${locallogdir}/files.txt
+sed -i "s/$sedfiledir\///g" ${locallogdir}/files.txt #filename only
+sed -i '/^$/d' ${locallogdir}/files.txt #remove whitespace
 find ${localfiledir}/ -maxdepth 1 -type d > ${locallogdir}/dirs.txt #list directories
-sed -i "s/$sedfiledir\///g" ${locallogdir}/dirs.txt
-sed -i '/^$/d' ${locallogdir}/dirs.txt
+sed -i "s/$sedfiledir\///g" ${locallogdir}/dirs.txt #dirname only
+sed -i '/^$/d' ${locallogdir}/dirs.txt #remove whitespace
 cat ${locallogdir}/files.txt ${locallogdir}/dirs.txt ${locallogdir}/nosyncfinal.txt > ${locallogdir}/nosync.txt #combine
 awk '!seen[$0]++' ${locallogdir}/nosync.txt > ${locallogdir}/nosyncfinal.txt #remove duplicates
 cp ${locallogdir}/nosyncfinal.txt ${locallogdir}/nosync_${date}.txt #log
