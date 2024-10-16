b92689b58f9653e51460382e0b90d2abd9c55d06
w-mccarty <94848975+w-mccarty@users.noreply.github.com>
2024-10-16 07:06:13 -0400

    Update compile.sh

--- ArduinoCLI/compile.sh
+++ ArduinoCLI/compile.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+while getopts ":s" option; do
+  case $option in
+    s)
+      vSCREEN="yes" #LAUNCH SCREEN 9600 BAUD
+      ;;
+    *)
+      vSCREEN="no"
+      ;;
+  esac
+done
+
 function compileSuccess() {
         echo "Compile SUCCEEDED"
         ~/bin/arduino-cli upload -p $1 --fqbn $2 $3
@@ -14,3 +27,6 @@ vPORT=$(printf "${vLIST}" | grep '/dev/tty*' | cut -f1 -d ' ')
 vFQBN=$(printf "${vLIST}" | grep 'arduino:*' | rev | cut -f2 -d ' ' | rev )
 vCORE=$(printf "${vLIST}" | grep 'arduino:*' | rev | cut -f1 -d ' ' | rev )
 ~/bin/arduino-cli compile --fqbn $vFQBN $vCOMP && compileSuccess $vPORT $vFQBN $vCOMP || compileFailed
+if [ "$vSCREEN" = "yes" ]; then
+        screen $vPORT 9600
+fi
