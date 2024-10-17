944de1aaff3a7e970d44f8692bcf0a2eacd9e1d0
w-mccarty <94848975+w-mccarty@users.noreply.github.com>
2024-10-16 21:06:57 -0400

    Update compile.sh

--- ArduinoCLI/compile.sh
+++ ArduinoCLI/compile.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
+###############################################################################################
+# FLAGS                                                                                       #
+#    -p /path/to/folder/    path to folder that holds .ino file (default current directory    #
+#    -b ####                baud (default 9600)                                               #
+#    -s                     launch screen serial with configured/default baud after upload    #
+###############################################################################################
 
-while getopts ":s" option; do
-  case $option in
-    s)
-      vSCREEN="yes" #LAUNCH SCREEN 9600 BAUD
-      ;;
-    *)
-      vSCREEN="no"
-      ;;
-  esac
+while getopts :sp:b: flag
+do
+        case "${flag}" in
+                p) vPATH=${OPTARG};;
+                b) vBAUD=${OPTARG};;
+                s) vSCREEN="yes";;
+        esac
 done
+if [ -z "$vPATH" ]; then
+        vCOMP=$PWD
+fi
+if [ -z "$vBAUD" ]; then
+        vBAUD=9600
+fi
+if [ -z "$vSCREEN" ]; then
+        vSCREEN="no"
+fi
 
 function compileSuccess() {
-        echo "Compile SUCCEEDED. Starting Upload..."
+        echo "Compile SUCCEEDED"
         ~/bin/arduino-cli upload -p $1 --fqbn $2 $3
 }
 
 function compileFailed() {
-        echo "Compile FAILED."
+        echo "Compile FAILED"
 }
 
-vCOMP=$PWD
-echo "Compiling ${vCOMP}..."
+echo "Compiling ${vCOMP}"
 vLIST=$(~/bin/arduino-cli board list | sed -n '2 p')
 vPORT=$(printf "${vLIST}" | grep '/dev/tty*' | cut -f1 -d ' ')
 vFQBN=$(printf "${vLIST}" | grep 'arduino:*' | rev | cut -f2 -d ' ' | rev )
 vCORE=$(printf "${vLIST}" | grep 'arduino:*' | rev | cut -f1 -d ' ' | rev )
 ~/bin/arduino-cli compile --fqbn $vFQBN $vCOMP && compileSuccess $vPORT $vFQBN $vCOMP || compileFailed
 if [ "$vSCREEN" = "yes" ]; then
-        screen $vPORT 9600
+        screen $vPORT $vBAUD
 fi
