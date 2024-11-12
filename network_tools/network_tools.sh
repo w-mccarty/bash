9a926f3e61ae645cb53f912977ed4dddd1deba98
w-mccarty <94848975+w-mccarty@users.noreply.github.com>
2024-11-11 23:12:30 -0500

    Update network_tools.sh

--- network_tools/network_tools.sh
+++ network_tools/network_tools.sh
@@ -1,5 +1,3 @@
-#!bin/bash
-
 cNOC='\033[0m'
 cRED='\033[0;31m'
 cORG='\033[0;33m'
@@ -13,7 +11,7 @@ f_deviceStats () {
     	echo "  2) Public ip Info"
     	echo "  3) Ping"
     	echo "  4) Serial"
-    	#echo "  5)Traceroute"
+    	echo "  5) Subnet"
 		echo " "
 		echo "  r) Refresh"
     	echo "  e) Exit"
@@ -28,8 +26,8 @@ f_deviceStats () {
 			f_ping ;;
 		4)
 			f_serial ;;
-		#5)
-		#	f_traceroute ;;
+		5)
+			f_subnet ;;
 		r)
 			f_deviceStats ;;
 		e)
@@ -96,6 +94,28 @@ f_deviceStats () {
 			printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 			f_ping
 		}
+		f_ping_custom () {
+			read -p "Enter IP or domain address to ping: " pIP
+                        dvr=$(fping -c4 -t500 -e -q $pIP 2>&1)
+                        dv0=$(echo $dvr | cut -d ':' -f 1)
+                        dv1=$(echo $dvr | cut -d ':' -f 2)
+                        if  [[ $dv1 =~ "," ]]; then
+                        	dv2=$(echo $dv1 | cut -d ',' -f 1 | sed 's/ //g')
+                        	dv3=$(echo $dv1 | cut -d ',' -f 2 | sed 's/ //g')
+                        else
+                        	dv2=$(echo $dv1 | sed 's/ //g')
+                                dv3=""
+                        fi
+                        if [[ $dv2 =~ "100%" ]]; then
+                        	dvc=$cRED
+                        elif [[ $dv2 =~ "0%" ]]; then
+                        	dvc=$cGRN
+                        else
+                        	dvc=$cORG
+                        fi
+                        echo -e "$dvc $pIP \t $dv0 \t $dv2 \t $dv3 $cNOC"
+			f_ping
+		}
 		echo "1) Quick Ping test"
 		echo "2) Custom Ping test"
 		echo "e) Exit"
@@ -110,6 +130,157 @@ f_deviceStats () {
 		else
 			f_ping
 		fi
+	}
+
+	f_subnet() {
+		read -p "Enter IPV4 address: " Var
+		read -p "Enter Mask: " Mas
+		dec_2_bin () {
+			D2B=({0..1}{0..1}{0..1}{0..1}{0..1}{0..1}{0..1}{0..1})
+			NC=($(echo "$1" | tr '.' '\n'))
+			BN=""
+			for j in $(seq 0 3);
+			do
+				BN="$BN${D2B[${NC[${j}]}]}"
+			done
+			echo $BN
+		}
+		spacer () {
+			BX=$1
+			BL="${#BX}"
+			BD=""
+			let BM=$((15 - BL))
+			for i in $(seq 1 $BM);
+			do
+				BD="$BD#"
+			done
+			BX="$BX$BD"
+			echo $BX | sed 's/#/ /g'
+		}
+		bin_2_dec () { #BINARY TO DECIMAL
+			NB=("0" "-24" "8" "-16" "16" "-8" "24" "32")
+			NA=$1 #name of binary var
+			NC=""
+			for i in $(seq 0 3);
+			do
+				NC="$NC$((2#${NA:${NB[$(($i * 2))]}:${NB[$((($i * 2) + 1))]}}))."
+			done
+			NC=${NC::-1}
+			if [[ $2 -eq 0 ]]; then #if $2 = 0, remove decimals and replace empty space with zeros
+				DV=($(echo "$NC" | tr '.' '\n'))
+				DA=""
+				for k in $(seq 0 3);
+				do
+					dlen="${DV[${k}]}"
+					dcen=${#dlen}
+					if [ $dcen -eq 2 ]; then
+						DC="0$dlen"
+					elif [ $dcen -eq 1 ]; then
+						DC="00$dlen"
+					else
+						DC=$dlen
+					fi
+					DA="$DA$DC"
+				done
+				NC=$DA
+			fi
+			echo $NC
+		}
+		ip_class () {
+			VBN=$(dec_2_bin $Var)
+			echo "$(spacer 'IP') - $(spacer $Var) - $VBN"
+			MBN=$(dec_2_bin $Mas)
+			echo "$(spacer 'Mask') - $(spacer $Mas) - $MBN"
+			res="${MBN//[^1]}"
+			res="${#res}"
+			hos=$((32-$res))
+			#NETWORK ADDRESS
+			NBIT=${VBN:0:-"${hos}"}
+			for i in $(seq 1 $hos);
+			do
+				NBIT="${NBIT}0"
+			done
+			GATEWAY=$(bin_2_dec $NBIT 1)
+			echo "$(spacer 'GATEWAY') - $(spacer $GATEWAY) - $NBIT"
+			#FIRST IP
+			NFIP="${NBIT::-1}1"
+			NFIC=$(bin_2_dec $NFIP 1)
+			echo "$(spacer 'FIRST IP') - $(spacer $NFIC) - $NFIP"
+			#LAST IP
+			NBIL=${VBN:0:-"${hos}"}
+			for i in $(seq 1 $hos);
+			do
+				NBIL="${NBIL}1"
+			done
+			NBIL="${NBIL::-1}0"
+			NBIC=$(bin_2_dec $NBIL 1)
+			echo "$(spacer 'LAST IP') - $(spacer $NBIC) - $NBIL"
+			#BROADCAST
+			NBIL=${VBN:0:-"${hos}"}
+			for i in $(seq 1 $hos);
+			do
+				NBIL="${NBIL}1"
+			done
+			NBIC=$(bin_2_dec $NBIL 1)
+			echo "$(spacer 'BROADCAST') - $(spacer $NBIC) - $NBIL"
+			#LIST CLASSES AND HOST NET BITS
+			ip=$(bin_2_dec $NBIT 0)
+			atype="public"
+			cbits=0
+			if [[ $ip -ge 001000000000 && $ip -le 127255255255 ]] ; then
+				if [[ $ip -ge 001000000000 && $ip -le 127255255255 ]] ; then
+					atype="private"
+				fi
+				class="A"
+				cbits=8
+			elif [[ $ip -ge 128000000000 && $ip -le 191255255255 ]] ; then
+				if [[ $ip -ge 172016000000 && $ip -le 172031255255 ]] ; then
+					atype="private"
+				fi
+				class="B"
+				cbits=16
+			elif [[ $ip -ge 192000000000 && $ip -le 223255255255 ]] ; then
+				if [[ $ip -ge 192168000000 && $ip -le 192168255255 ]] ; then
+					atype="private"
+				fi
+				class="C"
+				cbits=24
+			elif [[ $ip -ge 224000000000 && $ip -le 239255255255 ]] ; then
+				class="D"
+			else
+				class="E"
+			fi
+			let hbits=$((res - cbits))
+			echo " "
+			echo "/$res CIDR network bits with $hos host bits"
+			echo "        - class $class $atype ($cbits network + $hbits shared bits)"
+			let hbits=(2**$hbits)
+			echo "        - network(s): $hbits"
+			let bbits=(2**$hos)-2
+			echo "        - host(s) per network: $bbits"
+		}
+		#Check IP and MASK
+		if [[ "$Var" =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; then
+			MSP=($(echo "$Mas" | tr '.' '\n'))
+			List="255 254 252 248 240 224 192 128 0"
+			if [[ ${MSP[0]} < ${MSP[1]} || ${MSP[1]} < ${MSP[2]} || ${MSP[2]} < ${MSP[3]} ]]; then #test mask length
+				echo "mask not ok"
+			else
+				for i in $(seq 0 3); #test if mask val in List
+				do
+					if ! [[ $List =~ (^|[[:space:]])${MSP["${i}"]}($|[[:space:]]) ]]; then
+						echo "MASK NOT OK"
+						exit 0
+					else
+						continue
+					fi
+				done
+				ip_class
+			fi
+		else
+			echo "IP NOT OK"
+		fi
+		f_start
 	}
 
 	f_publicStats () {
